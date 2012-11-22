e38359b8862ae895987605bca2f196e1e1bf260f
Bill Piel <bill@billpiel.com>
2012-11-22 10:41:29 -0500

    add test-query endpoint and test

--- src/ferret_ql/views/ferret_ql_api.clj
+++ src/ferret_ql/views/ferret_ql_api.clj
@@ -1,9 +1,15 @@
 (ns ferret-ql.views.ferret-ql-api
   (:require [noir.core :refer [defpage]]
             [noir.statuses :refer [set-page!]]
-            [noir.response :as resp]))
+            [noir.response :as resp]
+            [ferret-ql.models.query-engine :refer :all]))
 
 (set-page! 404 "Page not found")
 
 (defpage testget "/testget/:id" {:keys [id]}
   (resp/json [:test id]))
+
+(defpage testquery "/test-query" {:keys [query data]}
+  (test-query query data))
+
+
--- test/ferret_ql/test/api_tests.clj
+++ test/ferret_ql/test/api_tests.clj
@@ -5,11 +5,27 @@
             [ferret-ql.views.ferret-ql-api  :refer :all]))
 
 (fact
-"Test fail"
-(+ 1 1)
-=>  3)
-
-(fact
  "Test testget"
  (:body (send-request "/testget/22"))
  => "[\"test\",\"22\"]")
+
+  (fact
+  "Test JSON query endpoint"
+  (:body (send-request "/test-query"
+    {:query
+    "{ \"select\": {\"sum\" : \"(sum (merge {x.a}))\",
+                  \"count\" : \"(count (merge {x.a}))\",
+                  \"b\":     \"(first {x.b})\"
+              },
+      \"for\" : \"x\",
+      \"in\" :  \"_\",
+      \"group\"  : [\"{x.b}\", \"{x.c}\"]
+    }"
+    :data
+    "[{\"a\": [1, 2],  \"b\": 1, \"c\": 1},
+      {\"a\": [3],    \"b\": 1, \"c\": 1},
+      {\"a\": [4, 5],  \"b\": 2, \"c\": 1},
+      {\"a\": [6],    \"b\": 2, \"c\": 2}]"}))
+
+  =>  "[{\"b\":1,\"sum\":6,\"count\":3},{\"b\":2,\"sum\":9,\"count\":2},{\"b\":2,\"sum\":6,\"count\":1}]")
+
