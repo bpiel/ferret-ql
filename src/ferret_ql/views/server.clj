17f17e8033d47e2db22e39770d58cdd6c3681be4
Bill Piel <bill@billpiel.com>
2012-11-22 08:57:10 -0500

    remove dup file

--- src/ferret_ql/views/server.clj
+++
@@ -1,9 +0,0 @@
-(ns ferret-ql.server
-  (:require [noir.server :as server]))
-
-(server/load-views-ns 'job-queue.views)
-
-(def server-handler
-  (server/gen-handler 
-    {:mode :prod
-     :ns 'job-queue}))
