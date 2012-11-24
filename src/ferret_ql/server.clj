e339bf3b2869e85ab7df7cb5069304970b1c4627
Bill Piel <bill@billpiel.com>
2012-11-24 15:11:53 -0500

    fix server.clj

--- src/ferret_ql/server.clj
+++ src/ferret_ql/server.clj
@@ -1,9 +1,9 @@
 (ns ferret-ql.server
   (:require [noir.server :as server]))
 
-(server/load-views-ns 'job-queue.views)
+(server/load-views-ns 'ferret-ql.views)
 
 (def server-handler
   (server/gen-handler 
     {:mode :prod
-     :ns 'job-queue}))
+     :ns 'ferret-ql}))
