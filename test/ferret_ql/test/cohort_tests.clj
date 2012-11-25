42541935e011f11ae977ece39165194b97f5eae0
Bill Piel <bill@billpiel.com>
2012-11-24 22:49:40 -0500

    cohort tests failing

--- test/ferret_ql/test/cohort_tests.clj
+++ test/ferret_ql/test/cohort_tests.clj
@@ -5,8 +5,53 @@
 
  (defmacro fact-skip [& r] `())
 
+(def orders-test-data [{"data-added" "2010-01-01"  "user_id" 1  "amount_total" 10.50   "acquisition_source" "AA"}
+                       {"data-added" "2010-01-03"  "user_id" 2  "amount_total" 5.20    "acquisition_source" "BB"}
+                       {"data-added" "2010-02-01"  "user_id" 1  "amount_total" 7.40    "acquisition_source" "AA"}
+                       {"data-added" "2010-03-01"  "user_id" 2  "amount_total" 14.00   "acquisition_source" "BB"}])
 
 (fact
-"+ 1 1"
-(+ 1 1)
-=>  3)
+  "acquisition source by user id"
+  (query-engine/test-squery
+    { "select" {"{order.user_id}"  { 
+        "source" "(first {order.acquisition_source})" 
+        }}
+      "for"   "order"
+      "in"    "_"
+      "group" "{order.user_id}"
+    }
+
+    orders-test-data)
+
+  => [])
+
+(fact
+  "aggragate orders by user id"
+  (query-engine/test-squery
+    { "select" {"{order.user_id}"  { 
+        "orders" "(merge {order.amount_total})"
+        }}
+      "for"   "order"
+      "in"    "_"
+      "group" "{order.user_id}"
+    }
+
+    orders-test-data)
+
+  => [])
+
+(fact
+  "orders and acquisition_source by user id"
+  (query-engine/test-squery
+    { "select" {"{order.user_id}"  { 
+        "source" "(first {order.acquisition_source})" 
+        "orders" ["{order.amount_total}"]
+        }}
+      "for"   "order"
+      "in"    "_"
+      "group" "{order.user_id}"
+    }
+
+    orders-test-data)
+
+  => [])
